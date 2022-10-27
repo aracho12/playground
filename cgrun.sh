58799b9e7e5c4f89264eb89f4bcbf789b7f2e49b
aracho12 <aracho@postech.ac.kr>
2022-10-28 08:59:32 +0900

    .

---
+++ cgrun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ----------- :: Information :: ----------------
+# By Ara Cho, POSTECH, Korea @09/14/2022
+# --------------- :: note :: -------------------
+# modification run file
+# ----------------------------------------------
+
+# -------------- set up --------------- #
+IFS='/' read -r -a scratch <<< "$SCRATCH"
+len=${#scratch[@]}
+IFS='/' read -r -a cpwd <<< "${PWD##}"
+proj=${cpwd[$len]}
+runfile='run_slurm.sh'
+runpath=$SCRATCH/$proj/script/$runfile
+
+
+sed -i 's/^[^#]*script/#&/' $runpath
+# if $1 exist
+if [[ -n $1 ]]; then
+	echo -e $bG'change to '$Y$bd$1$re$bG' setup'$dW
+	sed -i '/^#.*script.*'"$1"'/s/^#//' $runpath
+	grep --color $1 $runpath
+fi
