caf821bd68836197227b484649f6e14e93322af2
aracho12 <aracho@postech.ac.kr>
2022-10-29 10:44:12 +0900

    .

--- sub.sh
+++ sub.sh
@@ -53,7 +53,7 @@ bash $happy/srus.sh $name
 submit_info(){
 	vasp=$(grep /vasp. $runpath | grep -v "#" | rev | cut -d'/' -f1 | rev )
 	node=$(grep ntasks-per-node $runpath | cut -d'=' -f2)
-	numnode=$(grep --nodes= $runpath | cut -d'=' -f2)
+	numnode=$(grep nodes= $runpath | cut -d'=' -f2)
 	partition=$(grep partition $runpath | cut -d'=' -f2)
 	calpy=$(grep python $runpath | grep -v "#" | grep -v "if_finish_let_me_know.py" | gawk '{print $2}' | rev | cut -d'/' -f1 | rev)
 	name=$(grep "job-name=" $runpath | cut -d'=' -f2)
@@ -65,35 +65,43 @@ submit_info(){
 	echo -e "  vasp ver: $vasp"
 	echo -e "\033[1;31;03m—————————————————————————————"$dW
 }
+menu(){
+    echo -e "\033[93;03m————————————————————————"
+    echo -e "  cg) partition (g1, g2 ,g3, g4, g5, gpu)"
+    echo -e " cal) job type"
+    echo -e " cgn) number of node"
+    echo -e "   i) check changed info"
+	echo -e " cnt) check avaiable nodes"
+    echo -e " sub) let's submit job!"
+    echo -e "\033[93;03m————————————————————————$bW"
+}
+
 
 change(){
 	read -p ">> do you want to change something ?: " y
 	if [ "$y" == "y" ] ; then
-        echo -e "\033[93;03m————————————————————————"
-        echo -e " a) partition (g1, g2 ,g3, g4, g5, gpu)"
-        echo -e " b) job type"
-        echo -e " c) number of node"
-        echo -e " i) check changed info"
-		echo -e " s) let's submit job!"
-        echo -e "\033[93;03m————————————————————————$bW"	
+		menu
 		while :
 		do
 			read -p ">> choose option you want to change : " op
 			if [ "$op" == "a" ] || [ "$op" == "cg" ] ; then
 				bash $happy/sg.sh
-			elif [ "$op" == "b" ] || [ "$op" == "py" ]; then
+			elif [ "$op" == "b" ] || [ "$op" == "cgrun" ] || [ "$op" == "cal" ] ; then
         		bash $happy/joblist.sh
 		        read -p "$bW>> which calculation ?:" cal
 				bash $happy/cgrun.sh $cal
-			elif [ "$op" == "c" ] || [ "$op" == "node" ] ; then
+			elif [ "$op" == "c" ] || [ "$op" == "cgnode" ] || [ "$op" == "cgn" ] ; then
 				read -p "$bW>> how many node ?:" nn
 				bash $happy/cgnode.sh  $nn
+			elif [ "$op" == "cnt" ] ; then
+				bash $happy/t2.sh
 			elif [ "$op" == "i" ] ; then
 				submit_info
-			elif [ "$op" == "s" ] ; then
+			elif [ "$op" == "s" ] || [ "$op" == "sub" ] ; then
 				break
 			else
 				submit_info
+				menu
 			fi
 		done
 	fi
