222e857d6f56c5ded7c6273577ba3e1b307dc725
Ara Cho <aracho@postech.ac.kr>
2022-10-30 21:24:14 -0700

    .

--- alias-nersc.sh
+++ alias-nersc.sh
@@ -1,4 +1,5 @@
 alias qdel='scancel'
 alias vbash='vi ~/.bash_profile'
 alias sbash='source ~/.bash_profile'
-alias ssc='cd $SCRATH'
+alias viner='vi ~/bin/playground/alias-nersc.sh'
+#/home/aracho/8_suncat-orr/PES-model-for-ORR
