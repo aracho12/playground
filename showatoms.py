a3c4ba486ada5797bd304c8d78fff9ee617251e0
aracho12 <aracho@postech.ac.kr>
2022-11-07 18:46:47 +0900

    .

--- showatoms.py
+++ showatoms.py
@@ -1,11 +1,19 @@
+from sys import argv
 from ase.io import write, read
 import os.path
-
-if os.path.isfile('CONTCAR'):
+#print(argv[0])
+if len(argv) > 1:
+	inputfile=argv[1]
+	print("show:",inputfile)
+	slab = read(inputfile)
+elif os.path.isfile('CONTCAR'):
+	print("show: CONTCAR")
 	slab = read('CONTCAR')
 elif os.path.isfile("POSCAR"):
+	print("show: POSCAR")
 	slab = read("POSCAR")
 elif os.path.isfile("restart.json"):
+	print("show: restart.json")
 	slab = read("restart.json")
 else:
 	print("there is no input file")
