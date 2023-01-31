c83df354639e6248c159ffd36a30b9843a1abe80
aracho12 <aracho@postech.ac.kr>
2023-02-01 04:12:30 +0900

    .

--- alias-burning.sh
+++ alias-burning.sh
@@ -8,3 +8,10 @@ git push
 cd $dir_now'
 export server='burning'
 alias viburning='vi ~/bin/playground/alias-burning.sh'
+
+#VASP
+export VASP535=/TGM/Apps/VASP_OLD/bin/5.3.5/NORMAL
+export VASP632=/TGM/Apps/VASP/VASP_BIN/6.3.2
+
+export 535vtst=$VASP535/vasp.5.3.5_31MAR2014_GRP7_NORMAL_VTST.x
+export 632vaspsol=$VASP632/vasp.6.3.2.vaspsol.std.x
