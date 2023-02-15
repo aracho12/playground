4a3594dcff5ae7d9c078893d063032d06c7f2097
aracho12 <aracho12>
2023-02-16 04:32:47 +0900

    .

---
+++ alias-x2431.sh
@@ -0,0 +1,11 @@
+
+
+alias pestat='pbs_status | head -n 2
+pbs_status | grep normal
+pbs_status | grep norm_skl
+pbs_status | grep long'
+
+alias cprnf='bash $mc/cprn.sh final_with_calculator.json'
+alias cprn='bash $mc/cprn.sh'
+alias cdb='source $mc/cdback.sh'
+
