7f4dbec2fa76faeabeefc5017e4e952264c5bfd0
aracho12 <aracho@postech.ac.kr>
2023-02-16 04:40:29 +0900

    .

--- alias-burning.sh
+++ alias-burning.sh
@@ -13,5 +13,5 @@ alias viburning='vi ~/bin/playground/alias-burning.sh'
 export VASP535=/TGM/Apps/VASP_OLD/bin/5.3.5/NORMAL
 export VASP632=/TGM/Apps/VASP/VASP_BIN/6.3.2
 
-export 535vtst=$VASP535/vasp.5.3.5_31MAR2014_GRP7_NORMAL_VTST.x
-export 632vaspsol=$VASP632/vasp.6.3.2.vaspsol.std.x
+export vtst535=$VASP535/vasp.5.3.5_31MAR2014_GRP7_NORMAL_VTST.x
+export vaspsol632=$VASP632/vasp.6.3.2.vaspsol.std.x
