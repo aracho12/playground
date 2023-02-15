1d779b08e06d1eb28cfea180b84dc577c16ac952
aracho12 <aracho@postech.ac.kr>
2023-02-15 11:53:04 -0800

    .

--- alias-mac.sh
+++ alias-mac.sh
@@ -7,4 +7,10 @@ git commit -m "."
 git push
 cd $dir_now'
 alias vimac='vi $play/alias-mac.sh'
-
+alias dct='dir_now=$PWD
+cd /Users/aracho/Dropbox/0_Research/0_My_Publications/dct
+git pull
+git add *
+git commit -m "."
+git push
+cd $dir_now'
