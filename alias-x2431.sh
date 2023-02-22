3624ca6c9c0cc3ffdab74d79114c0002348820a9
aracho12 <aracho12>
2023-02-23 02:32:04 +0900

    .

--- alias-x2431.sh
+++ alias-x2431.sh
@@ -4,7 +4,13 @@ alias pestat='pbs_status | head -n 2
 pbs_status | grep normal
 pbs_status | grep norm_skl
 pbs_status | grep long'
-
+alias dct='dir_now=$PWD
+cd /scratch/x2431a03/dct
+git pull
+git add *
+git commit -m "."
+git push
+cd $dir_now'
 alias cprnf='bash $mc/cprn.sh final_with_calculator.json'
 alias cprn='bash $mc/cprn.sh'
 alias cdb='source $mc/cdback.sh'
