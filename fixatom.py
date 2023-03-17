c803fac32ebe50c59cf58591ba2ca6fa5a4b927d
aracho12 <aracho@postech.ac.kr>
2023-03-16 20:09:19 -0700

    fix atoms in slab

--- fixatom.py
+++ fixatom.py
@@ -1,3 +1,13 @@
+"""
+Ara Cho, Mar, 2023 @SUNCAT
+description: fix atoms in slab
+usage: python fixatom.py -i [filename] -f "element" -r "index" -d [distance]
+i.g) python fixatom.py -i POSCAR -f "Pt" # fix all Pt atoms
+    python fixatom.py -i POSCAR -r "41,42" # fix all except 41,42
+    python fixatom.py -i POSCAR -d 10.0 # fix all atoms below 10.0 Angstrom
+"""
+
+
 import sys
 from ase.io import read, write
 from ase.constraints import FixAtoms, FixedPlane
@@ -5,12 +15,22 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', help='input file', default='final_with_calculator.json')
-parser.add_argument('-f', '--fix', help='fix element', type=str)
-parser.add_argument('-r', '--relax', help='index for relax; others will be fixed', type=str)
+parser.add_argument('-f', '--fix', help='element to fix', type=str)
+parser.add_argument('-r', '--relax', help='index to relax; others will be fixed', type=str)
+parser.add_argument('-d', '--distance', help='all atoms will be fixed below distance', type=float)
 args = parser.parse_args()
 
 if args.input:
     print('Your inputfile is:', args.input)
+
+if args.fix:
+    print('fix element:', args.fix)
+    atoms=read(args.input)
+    fix_info = [atom.index for atom in atoms if atom.symbol==args.fix]
+    c=FixAtoms(indices=fix_info)
+    atoms.set_constraint(c)
+    write('restart.json',atoms)
+    print("{}: slab is successfully fixed".format(args.input))
 
 if args.relax:
     relax_index = [int(index) for index in args.relax.split(',')]
@@ -23,10 +43,10 @@ if args.relax:
     write('restart.json',atoms)
     print("{}: slab is successfully fixed".format(args.input))
 
-if args.fix:
-    print('fix element:', args.fix)
+if args.distance:
+    print('fix atoms below distance:', args.distance)
     atoms=read(args.input)
-    fix_info = [atom.index for atom in atoms if atom.symbol==args.fix]
+    fix_info = [atom.index for atom in atoms if atom.position[2]<args.distance]
     c=FixAtoms(indices=fix_info)
     atoms.set_constraint(c)
     write('restart.json',atoms)
