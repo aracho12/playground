e32a36f753ecd43dfdb492102ba4f12a05214ea7
aracho12 <aracho@postech.ac.kr>
2023-04-13 02:23:07 +0900

    .

--- alias.sh
+++ alias.sh
@@ -159,6 +159,7 @@ alias cgn='bash $happy/cgnode.sh'
 alias here='echo $server'
 alias scp.sh='bash $happy/scp.sh'
 alias repeat='bash $happy/repeat.sh'
+alias inneb.sh='bash $happy/inneb.sh'
 # --- Research --- #
 alias wf='python $happy/wf_cal.py'
 alias getrst='python $happy/get_restart.py'
