d2f95afecd85867a1163d755788082677437819d
aracho12 <aracho@postech.ac.kr>
2023-05-02 22:39:36 -0700

    .

--- sub.sh
+++ sub.sh
@@ -16,7 +16,7 @@ job_name=${jobname2[1]}
 
 source $happy/here.sh
 
-if [[ -n $1 ]] ; then
+if [[ -n $1 ]] || [[ -z $2 ]] ; then
 	if [ $1 == '-r' ] ; then
 		if test -e "run_submit.sh" ; then
 			name=$(grep "job-name=" run_submit.sh | cut -d'=' -f2 | tr -d '"')
