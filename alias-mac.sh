e39f632dc3a0d0ad548b74a7afb27ab0b0ef0c5b
aracho12 <aracho@postech.ac.kr>
2023-06-16 16:32:21 -0700

    .

--- alias-mac.sh
+++ alias-mac.sh
@@ -9,3 +9,5 @@ cd $dir_now'
 export pes=/Users/aracho/Dropbox/0_Research/0_My_Publications/24_2023_PES-model/nersc
 
 alias pes='$pes/backup aracho@perlmutter-p1.nersc.gov:/pscratch/sd/a/aracho/01_Pt_ORR_kinetic/. $pes/.'
+
+alias eoga='open *.png'
