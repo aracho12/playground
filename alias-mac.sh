f9be23e62ca3c762ee07f9e46d0a234727c633fe
aracho12 <aracho@postech.ac.kr>
2023-08-07 00:42:47 -0700

    .

--- alias-mac.sh
+++ alias-mac.sh
@@ -11,3 +11,5 @@ export pes=/Users/aracho/Dropbox/0_Research/0_My_Publications/24_2023_PES-model/
 alias pes='$pes/backup aracho@perlmutter-p1.nersc.gov:/pscratch/sd/a/aracho/01_Pt_ORR_kinetic/. $pes/.'
 
 alias eoga='open *.png'
+alias jl='jupyter-lab --no-browser'
+
