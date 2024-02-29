5fe3c06d7a737a23a0938f660cea9eb85a28a5be
aracho12 <aracho@postech.ac.kr>
2024-02-28 19:25:12 -0800

    .

--- alias-sdf.sh
+++ alias-sdf.sh
@@ -1,3 +1,4 @@
 alias fstat='date -d yesterday |sacct --format="JobID,JobName%30,Partition,Elapsed,NCPUS,State" | grep roma | grep -e PENDING -e RUNNING -e COMPLETED -e TIMEOUT'
 alias cpsn='bash $happy/sdf/cp_file_sync_sdf.sh'
 alias rmsn='bash $happy/sdf/del_file_in_sdf.sh'
+alias cpvp='cp ~/bin/vasp/* .'
