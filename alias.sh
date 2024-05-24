91cafd61b8b505059647cb00965a79bad0660dc9
aracho12 <aracho@postech.ac.kr>
2024-05-24 11:49:08 -0700

    .

--- alias.sh
+++ alias.sh
@@ -243,6 +243,7 @@ alias rerun='bash $happy/execute_previous_command.sh'
 alias mkfol='bash $happy/sdf/mk_active_sites_dirs.sh'
 alias qs='bash $happy/qstat.sh'
 alias plotcov='python $happy/plots/catmap_plot_coverage.py'
+alias plotsel='python $happy/plots/catmap_plot_selectivity.py'
 
 # --- PtORR --- #
 alias ptwater='python $happy/pt-water.py'
