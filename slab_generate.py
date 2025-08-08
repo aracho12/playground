634aee4cda656e872aae2e52918de8b67fa4211e
Ara Cho <43310503+aracho12@users.noreply.github.com>
2025-08-08 16:15:52 -0700

    Update slab_generate.py

--- slab_generate.py
+++ slab_generate.py
@@ -1,4 +1,5 @@
 from ase.build import fcc111, surface, bulk
+from ase.atoms import Atoms
 from ase.io import write
 from ase.io import read
 
@@ -10,17 +11,20 @@ if len(sys.argv) < 2:
 
 atoms = read(sys.argv[1])
 
-slab = surface(atoms, (1,1,1), 4) #(lattice, indices, layers, vacuum=None, tol=1e-10, periodic=False)
-slab.center(vacuum=10.0, axis=2)
+lattice = 4.0
+atoms = Atoms('Ni3Cu',    scaled_positions=[(0, 0, 0), (0.5, 0.5, 0), (0.5, 0, 0.5), (0, 0.5, 0.5)],
+    cell=[lattice, lattice, lattice],
+    pbc=True)
+slab = surface(atoms, (1, 0, 0), 2)
+slab.center(vacuum=10, axis=2)
 
 # supercell
-sc = slab * (3,3,1)
+sc = slab * (2,2,1)
 
 write('slab.traj',sc)
 
 #slab = fcc111('Pt', size=(3,3,4), vacuum=8, orthogonal=False, periodic=True)
 #write('slab.json',slab)
-
 
 
 
