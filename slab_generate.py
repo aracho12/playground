33a7c23274fa6f895c8b1393ded6aef16cc92bfc
Ara Cho <43310503+aracho12@users.noreply.github.com>
2025-08-08 15:50:43 -0700

    Update slab_generate.py

--- slab_generate.py
+++ slab_generate.py
@@ -1,5 +1,22 @@
-from ase.build import fcc111
+from ase.build import fcc111, surface, bulk
 from ase.io import write
+from ase.io import read
 
-slab = fcc111('Pt', size=(3,3,4), vacuum=8, orthogonal=False, periodic=True)
-write('slab.json',slab)
+# read atoms from command line
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python slab_generate.py <atoms.json>")
+    sys.exit()
+
+atoms = read(sys.argv[1])
+
+slab = surface(atoms, miller_indices=(1,1,1), layers=4)
+slab.center(vacuum=10.0, axis=2)
+
+write('slab.traj',slab)
+#slab = fcc111('Pt', size=(3,3,4), vacuum=8, orthogonal=False, periodic=True)
+#write('slab.json',slab)
+
+
+
+
