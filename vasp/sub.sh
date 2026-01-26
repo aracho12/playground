f75601aedf158005677304ff65b6e42efa10277a
Ara Cho <43310503+aracho12@users.noreply.github.com>
2026-01-26 17:04:46 +0900

    sub

--- vasp/sub.sh
+++ vasp/sub.sh
@@ -1,5 +1,11 @@
 runfile='run_slurm.sh'
 
+# 만약에 '#SBATCH -J JOB_NAME' 이 있으면, input 받아서 modify_runfile.sh -j 로 수정 후 제출
+if grep -q "#SBATCH -J JOB_NAME" "$runfile"; then
+    read -p "Enter job name: " job_name
+    bash $play/vasp/modify_runfile.sh -j "$job_name" "$runfile"
+fi
+
 if [ -f $runfile ]; then
     sbatch $runfile
 else
