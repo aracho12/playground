bf0e9beb1dcdac806ca537b50c0f3a9e4870f41e
aracho12 <aracho@postech.ac.kr>
2026-04-09 00:36:58 +0900

    -

--- go.sh
+++ go.sh
@@ -9,7 +9,7 @@ echo "JOBID: $1"
 if [ "$FIRST_CHAR" = "x" ]; then
     # PBS scheduler (username starts with 'x')
     workdir=$(qstat -f "$1" 2>/dev/null \
-        | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' \
+        | perl -pe 's/\e\[[0-9;]*[a-zA-Z]//g' \
         | sed ':a;N;$!ba;s/\n[[:space:]]\+//g' \
         | grep -o 'PBS_O_WORKDIR=[^,]*' \
         | cut -d'=' -f2 \
