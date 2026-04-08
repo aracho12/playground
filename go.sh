6ec642d0869d985057126967ae32cebcd630a269
aracho12 <aracho@postech.ac.kr>
2026-04-09 00:12:25 +0900

    strip ANSI escape codes from qstat output before path extraction

--- go.sh
+++ go.sh
@@ -9,9 +9,12 @@ echo "JOBID: $1"
 if [ "$FIRST_CHAR" = "x" ]; then
     # PBS scheduler (username starts with 'x')
     workdir=$(qstat -f "$1" 2>/dev/null \
+        | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' \
         | sed ':a;N;$!ba;s/\n[[:space:]]\+//g' \
         | grep -o 'PBS_O_WORKDIR=[^,]*' \
-        | cut -d'=' -f2)
+        | cut -d'=' -f2 \
+        | tr -d '\r' \
+        | sed 's/[[:space:]]*$//')
 
     if [ -z "$workdir" ]; then
         echo "JobID $1 not found"
